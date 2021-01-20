41c7e39713960220eb984ec3494d9902753c9f94
Christian Koski <64987609+ckosk@users.noreply.github.com>
2021-01-20 12:41:57 -0600

    Update mouseXY.py
    Added minor "quality of life" changes. 
    -Shell now stays in the foreground.
    -Shell moves into top left corner and decreases in size for ease-of-use
    
    Known bugs as of 1/20/21
    -Can occasionally crash when minimizing an application behind the shell window.

--- mouseXY.py
+++ mouseXY.py
@@ -1,7 +1,14 @@
 import pyautogui, sys
+import pygetwindow as gw 
+
+win = gw.getWindowsWithTitle('Command Prompt')[0]
+win.resizeTo(400, 200)
+win.moveTo(10,10) 
+
 print('Press Ctrl-C to quit.')
 try:
     while True:
+        win.activate()
         x, y = pyautogui.position()
         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
         print(positionStr, end='')
